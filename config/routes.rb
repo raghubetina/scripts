b8b237871cacbae45f2b8142eaf2ef8e9ae380cd
Raghu Betina <raghu@firstdraft.com>
2017-05-11 15:44:24 +0000

    Generate vignettes resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Vignette resource:
+  # CREATE
+  get "/vignettes/new", :controller => "vignettes", :action => "new"
+  post "/create_vignette", :controller => "vignettes", :action => "create"
+
+  # READ
+  get "/vignettes", :controller => "vignettes", :action => "index"
+  get "/vignettes/:id", :controller => "vignettes", :action => "show"
+
+  # UPDATE
+  get "/vignettes/:id/edit", :controller => "vignettes", :action => "edit"
+  post "/update_vignette/:id", :controller => "vignettes", :action => "update"
+
+  # DELETE
+  get "/delete_vignette/:id", :controller => "vignettes", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
   # READ
